c071c828d46455b240fa4b1877198e459129b3dd
oneup40 <oneup40@gmail.com>
2017-05-20 20:11:35 +0000

    add tests for tell

---
+++ test/test_tell.py
@@ -0,0 +1,26 @@
+import os, shutil, sys, tempfile, unittest
+from pathlib import Path
+
+from chunkfile import *
+
+class TestChunkFileSeek(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = Path(tempfile.mkdtemp())
+
+    def tearDown(self):
+        shutil.rmtree(str(self.tmpdir))
+
+
+    def testTellMultipleChunk(self):
+        f = ChunkFile.open(self.tmpdir, 'wb')
+
+        x1M = b'x' * 1024 * 1024
+        written = 0
+
+        while written < CHUNKDATASIZE * 3:
+            f.write(x1M)
+            written += len(x1M)
+            self.assertEqual(f.tell(), written)
+
+if __name__ == '__main__':
+    unittest.main()
